87788a99b2f8373e3a6aadd511ed26981b226bb2
Nyasha <nashmutazu@gmail.com>
2020-07-31 22:44:44 +0100

    third commit

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,15 @@
-const express = require("express"), bodyParser = require("body-parser"), path = require("path"), mongoose = require("mongoose"), cors = require("cors"), passport = require("passport")
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  path = require("path"),
+  mongoose = require("mongoose"),
+  cors = require("cors"),
+  passport = require("passport");
 
 const indexRoutes = require("./routes/index");
 
 require("./config/passport");
 
-const environment = require("./environment/environment-prod");
+const url = require("./environment/environment-prod");
 
 const app = express();
 
@@ -15,12 +20,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
 
+app.use(express.static(__dirname + "/ReadForum"));
+
 app.use("/api", indexRoutes);
 
-app.use(express.static(__dirname + "./ReadForum"))
-
 mongoose
-  .connect(environment.DATABASE_URL)
+  .connect(url.DATABASE_URL)
   .then(response => {
     if (production) {
       console.log("Successfully connected to database");
@@ -28,13 +33,13 @@ mongoose
   })
   .catch(err => {
     if (production) {
-      console.log("Failed to connect to database");
-      console.log(err)
+      console.log("Failed to connect to database"); 
+      console.log(err);
     }
   });
 
-  app.get("/*", (req, res, next) => {
-    res.sendFile(path.join(__dirname + "./ReadForum/index.html"))
-  })
+app.get("/*", (req, res, next) => {
+  res.sendFile(path.join(__dirname + "/ReadForum/index.html"));
+});
 
 module.exports = app;
